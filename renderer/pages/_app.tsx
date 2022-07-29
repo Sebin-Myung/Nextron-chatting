276e11f2bcd3fd243de9b99e468bb4cf3c704923
Sebin-Myung <wrqewrq213@naver.com>
2022-07-29 23:54:16 +0900

    Chore : firebase 연동, .prettierrc 추가

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
-
-import '../styles/globals.css';
+import React from "react";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "../config/config";
+import { getDatabase } from "firebase/database";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+
   return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
