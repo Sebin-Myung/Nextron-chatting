0f7a119a47eb9e8f57f2b976b7af81c610634a25
Sebin-Myung <wrqewrq213@naver.com>
2022-07-31 19:15:38 +0900

    Design : BottomAlert width 수정

--- renderer/components/BottomAlert.tsx
+++ renderer/components/BottomAlert.tsx
@@ -1,9 +1,10 @@
 const BottomAlert = ({ message, visibility }: { message: string; visibility: boolean }) => {
   return (
     <div
-      className={`fixed inset-x-0 bottom-0 m-5 alert justify-center bg-rose-100 shadow-lg duration-500 ${
+      className={`fixed right-1/2 translate-x-1/2 bottom-4 alert justify-center bg-rose-100 shadow-lg duration-500 ${
         visibility ? "visible opacity-100" : "invisible opacity-0"
       }`}
+      style={{ width: "calc(100% - 2rem)" }}
     >
       <div>
         <span>{message}</span>
